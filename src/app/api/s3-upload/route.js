dff23ccc29dc719657d0a47ac050032ced7908c8
Raddy <rx3mer@live.co.uk>
2023-11-20 17:43:37 +0000

    Do not use NEXT_PUBLIC_

--- src/app/api/s3-upload/route.js
+++ src/app/api/s3-upload/route.js
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-	region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
+	region: process.env.AWS_S3_REGION,
 	credentials: {
-		accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID,
-		secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY,
+		accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
 	}
 });
 
@@ -16,7 +16,7 @@ async function uploadFileToS3(file, fileName) {
 	console.log(fileName);
 
 	const params = {
-		Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+		Bucket: process.env.AWS_S3_BUCKET_NAME,
 		Key: `${fileName}`,
 		Body: fileBuffer,
 		ContentType: "image/jpg"
